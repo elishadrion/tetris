9b89795b8b65bcb10f8e77a1c06ed9aabe9471c8
Detobel36 <detobel36@hotmail.fr>
2016-02-07 10:40:41 +0100

    Correction alignament connection.cpp Ajout d'un type de fichié dépendant de codeblock dans les fichiers ignorées

--- app/server/Connection.cpp
+++ app/server/Connection.cpp
@@ -1,7 +1,7 @@
 #include "Connection.hpp"
 
-struct addrinfo* Connection::get_client_info(void) const {
-    struct addrinfo *client_info;
+struct addrinfo* Connection::get_machine_info(void) const {
+    struct addrinfo *machine_info;
     struct addrinfo suggestions;		  /* Main data structure */
     memset(&suggestions, 0, sizeof(suggestions));
     suggestions.ai_family = AF_INET6;	 /* IPv4 not IPv6 */
@@ -9,42 +9,44 @@ struct addrinfo* Connection::get_client_info(void) const {
     suggestions.ai_socktype = SOCK_STREAM; /* TCP connection oriented */
 
     int ecode;
-    if((ecode = getaddrinfo(NULL, PORT, &suggestions, &client_info)) != 0) {
-	printf("server - Failed getting address information: %s\n" , gai_strerror(ecode));
-	exit(EXIT_FAILURE);
+    if((ecode = getaddrinfo(NULL, PORT, &suggestions, &machine_info)) != 0) {
+        printf("server - Failed getting address information: %s\n" , gai_strerror(ecode));
+        exit(EXIT_FAILURE);
     }
 
-    return client_info;
+    return machine_info;
 }
 
-int Connection::prepare_socket(struct addrinfo* client_info) const {
+int Connection::prepare_socket(struct addrinfo* machine_info) const {
     int sockfd;
-    if ((sockfd = socket(client_info -> ai_family,
-			 client_info -> ai_socktype,
-			 client_info -> ai_protocol)) == -1) {
-	perror("server - Failed creating socket");
-	/* and print last error encountered */
-	exit(EXIT_FAILURE);
+    if ((sockfd = socket(machine_info -> ai_family,
+			 machine_info -> ai_socktype,
+			 machine_info -> ai_protocol)) == -1) {
+
+        perror("server - Failed creating socket");
+        /* and print last error encountered */
+        exit(EXIT_FAILURE);
     }
 
     int reuse_addr_to_true = 1;
     if (setsockopt(sockfd, SOL_SOCKET,
 		   SO_REUSEADDR, &reuse_addr_to_true, sizeof(int)) == -1) {
-	perror("server - Failed setting reuse address option for the socket");
-	exit(EXIT_FAILURE);
+
+        perror("server - Failed setting reuse address option for the socket");
+        exit(EXIT_FAILURE);
     }
 
-    if (bind(sockfd, client_info -> ai_addr, client_info -> ai_addrlen) == -1) {
-	close(sockfd);
-	perror("server - Failed to assign addr to socket file descriptor");
-	exit(EXIT_FAILURE);
+    if (bind(sockfd, machine_info -> ai_addr, machine_info -> ai_addrlen) == -1) {
+        close(sockfd);
+        perror("server - Failed to assign addr to socket file descriptor");
+        exit(EXIT_FAILURE);
     }
 
-    freeaddrinfo(client_info); 	/* No need for this anymore */
+    freeaddrinfo(machine_info); 	/* No need for this anymore */
 
     if (listen(sockfd, BACKLOG)) {
-	perror("server - Could not mark the socket referenced by sockfd as passive");
-	exit(EXIT_FAILURE);
+        perror("server - Could not mark the socket referenced by sockfd as passive");
+        exit(EXIT_FAILURE);
     }
 
     return sockfd;
@@ -56,8 +58,16 @@ void* handler(void* new_fd) {
     // sockfd to communicate with client.
     int sockfd = *(int*)new_fd;
 
-
     // Game, login, etc... different calls
+    // Example: stream server printing what the clients are saying
+
+    int numbytes;
+    char buf[MAXDATASIZE];
+    while ((numbytes = recv(sockfd, buf, MAXDATASIZE, 0))) {
+        buf[numbytes] = '\0';
+
+        printf("server - received '%s'\n", buf);
+    }
 
     close(sockfd);
     return 0;
@@ -65,26 +75,27 @@ void* handler(void* new_fd) {
 
 void Connection::mainloop() {
     // setup
-    int sockfd = prepare_socket(get_client_info());
+    int sockfd = prepare_socket(get_machine_info());
 
     // loop
     while (1) {
-	int new_fd;		// where stuff happens
-	sin_size = sizeof guest;
-	if ((new_fd = accept(sockfd, (struct sockaddr *)&guest, &sin_size)) == -1) {
-	    perror("server - Error extracting connection request");
-	    exit(EXIT_FAILURE);
-	}
+        int new_fd;		// where stuff happens
+        sin_size = sizeof guest;
+        printf("Listening on socket %i ..... | ", sockfd);
+        if ((new_fd = accept(sockfd, (struct sockaddr *)&guest, &sin_size)) == -1) {
+            perror("server - Error extracting connection request");
+            exit(EXIT_FAILURE);
+        }
 
-	char from[INET6_ADDRSTRLEN];
-	inet_ntop(AF_INET6, &(guest.sin6_addr), from, INET6_ADDRSTRLEN);
-	printf("server - Got connection from: %s\n", from);
+        char from[INET6_ADDRSTRLEN];
+        inet_ntop(AF_INET6, &(guest.sin6_addr), from, INET6_ADDRSTRLEN);
+        printf("server - Got connection from: %s\n", from);
 
-	if (pthread_create(&thread_id, NULL, handler, &new_fd) < 0) {
-	    perror("server - Could not create thread");
-	    exit(EXIT_FAILURE);
-	}
-	close(sockfd);
-	pthread_join(thread_id , NULL);
+        if (pthread_create(&thread_id, NULL, handler, &new_fd) < 0) {
+            perror("server - Could not create thread");
+            exit(EXIT_FAILURE);
+        }
+
+        pthread_join(thread_id , NULL);
     }
 }
