18fecdb5a98e4ae2c0f90584e97a6963b8e01036
Corentin <ccandeur@ulb.ac.be>
2016-02-12 17:24:49 +0100

    Changing Player.hpp

--- app/server/Player.hpp
+++ app/server/Player.hpp
@@ -1,10 +1,3 @@
-/*
- * File:   Player.hpp
- * Author: Detobel36
- *
- * Created on 2 février 2016, 20:41
- */
-
 #ifndef PLAYER_HPP
 #define	PLAYER_HPP
 
@@ -21,45 +14,27 @@
  */
 class Player {
 
-    static std::vector<Player> allPlayer;
-
-
     int _sockfd;
     std::string _username;
     std::string _pass;
     int _victory;
     int _defeat;
-    Collection _collection;
     std::vector<Deck*> _listDeck;
-    bool _online;
 
 protected:
     std::vector<Deck*> getListDeck() {return _listDeck;}
 
-
 public:
-    Player() {} // TO DO
     // Create the player (and open the file (if it exist) to complete the informations)
     // Stock the new player in the allPlayer list
-    Player(int sockfd, std::string username);
-
-    virtual bool login(std::string pass) {return true;} // Try to login
-
+    Player(int sockfd, int playerID);
     virtual ~Player() = default;
 
-
-    ///// STATIC /////
-    static Player getPlayer(std::string username);
-    static Player createPlayer(std::string username, std::string password);
-
-
 private:
-    virtual void save() {} // Save the player in a file TO DO
-
+    virtual void save() const;
 
 };
 
 
 
 #endif	/* PLAYER_HPP */
-
