3f6d6dd578df1cc7b65590b79c0c7bee258f7fc1
DETOBEL Remy <rdetobel@romeo34.ulb.ac.be>
2016-02-23 13:54:05 +0100

    Début du collection

--- app/server/Collection.hpp
+++ app/server/Collection.hpp
@@ -6,13 +6,30 @@
 class Card;
 
 class Collection {
-
-    std::vector<Card*> _listAllCards;
 public:
 
     Collection(){};
     ~Collection(){};
-    void add(Card*);
+    //void add(Card*);
+
+
+
+    std::vector<*Card>_listCard;
+
+    // TO DO
+
+private:
+    void removeCard(int i); // removes the i-th card from the deck
+    void removeCard(Card*); // removes a card from the deck
+    int indexOfCard(int id); // the index of a card
+    int getCardIndex(Card*); // get the index of the card
+
+public:
+    Collection(const Collection&); // Copy constructor
+    Collection& operator=(const Collection&); // Copy operator
+    void addCard(Card*); // adds a card in the deck
+    void addCard(int cardId); // add a card in the collection with the id
+
 
 };
 
