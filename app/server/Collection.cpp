c4fd13cbc74cd9543f46ff06ddc9452e896e85eb
Detobel36 <detobel36@hotmail.fr>
2016-02-23 20:45:29 +0100

    Début de collection

--- app/server/Collection.cpp
+++ app/server/Collection.cpp
@@ -1,5 +1,114 @@
 #include "Collection.hpp"
 
-void Collection::add(Card* card){
-    _listAllCards.push_back(card);
+
+/**
+ * Default constructor
+ */
+Collection::Collection() {}
+
+/**
+ * Constructor
+ *
+ * @param listCard list of card
+ */
+Collection::Collection(std::vector<Card*> listCard):
+    _listCard(listCard) {}
+
+/**
+ * Constructor
+ *
+ * @param listIdCard the list of card ID
+ */
+Collection::Collection(std::vector<int> listIdCard) {
+    for(unsigned int i = 0; i < listIdCard.size(); ++i) {
+        _listCard.push_back(CardManager::getCardById(listIdCard[i]));
+    }
+}
+
+/**
+ * Copy constructor
+ *
+ * @param collection to copy
+ */
+Collection::Collection(const Collection& collection):
+    _listCard(collection._listCard) {}
+
+
+/**
+ * Copy opperator
+ *
+ * @param col collection to copy
+ */
+Collection& Collection::operator=(const Collection& col) {
+    _listCard = col._listCard;
+}
+
+
+/**
+ * Add a Card to the collection
+ *
+ * @param card the card to add
+ */
+void Collection::addCard(Card* card) {
+    _listCard.push_back(card);
+}
+
+
+/**
+ * Add a Card to the collection
+ *
+ * @param cardId the id of the card
+ */
+void Collection::addCard(int cardId) {
+    _listCard.push_back(CardManager::getCardById(cardId));
+}
+
+
+/**
+ * Remove a Card on a specific position
+ *
+ * @param i index of the card
+ */
+void Collection::removeCard(int i) {
+    _listCard.erase(_listCard.begin() + i);
+}
+
+
+/**
+ * Remove a Card from the collection
+ *
+ * @param card the card to remove
+ */
+void Collection::removeCard(Card* card) {
+    _listCard.erase(
+        std::remove(_listCard.begin(), _listCard.end(), card),
+        _listCard.end());
+}
+
+
+/**
+ * Remove a Card from the collection
+ *
+ * @param cardId the id of the card
+ */
+void Collection::removeCardId(int cardId) {
+    removeCard(CardManager::getCardById(cardId));
+}
+
+
+/**
+ * Get the index of one card
+ *
+ * @param card
+ * @return index or -1 if not found
+ */
+int Collection::getCardIndex(Card* card) {
+    int res = -1;
+
+    std::vector<Card*>::iterator it = std::find(_listCard.begin(), _listCard.end(), card);
+    if(it != _listCard.end()) {
+        res = std::distance(_listCard.begin(), it);
+    }
+
+    return res;
 }
--- app/server/Collection.hpp
+++ app/server/Collection.hpp
@@ -3,34 +3,30 @@
 #define COLLECTION_HPP
 
 #include <vector>
+
+#include "CardManager.hpp"
+
+
 class Card;
+#include "Card.hpp"
 
 class Collection {
 public:
-
-    Collection(){};
-    ~Collection(){};
-    //void add(Card*);
-
-
-
-    std::vector<*Card>_listCard;
-
-    // TO DO
-
-private:
-    void removeCard(int i); // removes the i-th card from the deck
-    void removeCard(Card*); // removes a card from the deck
-    int indexOfCard(int id); // the index of a card
-    int getCardIndex(Card*); // get the index of the card
+    std::vector<Card*>_listCard;
 
 public:
+    Collection();
+    Collection(std::vector<Card*>);
+    Collection(std::vector<int>);
     Collection(const Collection&); // Copy constructor
     Collection& operator=(const Collection&); // Copy operator
     void addCard(Card*); // adds a card in the deck
     void addCard(int cardId); // add a card in the collection with the id
-
-
+    void removeCard(int i); // removes the i-th card from the deck
+    void removeCard(Card*); // removes a card from the deck
+    void removeCardId(int cardId);
+    int indexOfCard(int cardId); // the index of a card
+    int getCardIndex(Card*); // get the index of the card
 };
 
 #endif  /* COLLECTION_HPP */
