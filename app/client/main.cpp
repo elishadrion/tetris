10d816abcfe6b6865d92a45fcc13c18aa7b3c388
Detobel36 <detobel36@hotmail.fr>
2016-03-04 11:43:47 +0100

    Ajout d'argument pour choisir le CLI ou le GUI

--- app/client/main.cpp
+++ app/client/main.cpp
@@ -14,6 +14,21 @@ Connection *conn;
 Display *display;
 CacheManager *cacheManager;
 
+/**
+ * View if argument is a GUI information or an IP adresse
+ *
+ * @param arg argument whitch must be test
+ * @return True if it's an IP
+ */
+bool isIP(std::string arg) {
+    for(unsigned int i=0; i < arg.size(); i++) {
+        arg[i] = std::toupper(arg[i]);
+    }
+
+    return arg != "GUI" && arg != "CLI";
+}
+
+
 int main(int argc, char** argv) {
     /* We initialise the static logger system
      * If it fail, client must to stop because log is important !
@@ -27,31 +42,64 @@ int main(int argc, char** argv) {
 
     WizardLogger::info("Starting client");
     
+    std::string adress = "localhost";
+    bool GUI = true;
+    if(argc > 1) {
+        std::string arg1 = argv[1];
+        if(isIP(arg1)) {
+            adress = arg1;
+        } else {
+            GUI = (arg1 == "GUI");
+        }
+
+        if(argc > 2) {
+            std::string arg2 = argv[2];
+            if(isIP(arg2)) {
+                if(adress == "localhost") {
+                    adress = arg2;
+                } else {
+                    std::string warnMsg = "Vous aviez déjà définit";
+                    warnMsg += " l'adresse dans l'argument 1";
+                    WizardLogger::warning(warnMsg);
+                }
+            } else {
+                GUI = (arg2 == "GUI");
+            }
+        }
+    }
+
+
     /* We initialise the connection between client and server
      * By the way, we initialise tchat socket
      * If agument is provide, use it as hostName, if not we try localhost
      * If it fail, client can't go farther
      */
     try {
-        argc > 1 ? conn = new Connection(argv[1]) : conn = new Connection((char*)"localhost");
+        conn = new Connection((char*) adress.c_str());
     } catch (...) {
         std::cerr << "Communication impossible avec le serveur (voir log)" << std::endl;
         return EXIT_FAILURE;
     }
-    
+
     /* We init the cacheManager */
     cacheManager = new CacheManager();
-    
-    /* We init in CLI mode
-     * If it fail, client must stop
-     */
-    try {
-        display = new CLI();
-    } catch (...) {
-        std::cerr << "Impossible de créer l'interface (voir log)" << std::endl;
-        return EXIT_FAILURE;
+
+
+    if(GUI) {
+        // TO DO
+    } else {
+        /* We init in CLI mode
+         * If it fail, client must stop
+         */
+        try {
+            display = new CLI();
+        } catch (...) {
+            std::cerr << "Impossible de créer l'interface (voir log)" << std::endl;
+            return EXIT_FAILURE;
+        }
     }
-    
+
+
     /* Display login prompt
      * Only call it after all backround service are ready
      * User become the only one to can modify program state
