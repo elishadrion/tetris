6406eb3dce4b9efbc87a88e7ee4823bc36c57969
abbouali <33732175+abbouali@users.noreply.github.com>
2018-03-05 22:54:22 +0100

    Update main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     
-	//Client my_client(std::string("127.0.0.1"));
-    Client my_client(std::string("109.129.213.199"));
+	Client my_client(std::string("127.0.0.1"));
+    	//Client my_client(std::string("109.129.213.199"));
 	my_client.start();
 	return 0;
 }
